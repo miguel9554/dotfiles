3eb80f96a1041d355230e4456bc99b4c217628ab
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2026-02-15 16:14:59 -0300

    current

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,10 @@ function fish_user_key_bindings
   fzf_key_bindings
 end
 
+set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --no-ignore-parent -g "!.git/"'
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden --follow'
+
 fish_add_path ~/bin
 fish_add_path ~/.local/bin
 
@@ -18,3 +22,16 @@ fish_add_path ~/.local/bin
 
 set -Ux EDITOR vim
 set -Ux _JAVA_AWT_WM_NONREPARENTING 1
+fnm env --use-on-cd | source
+
+set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+function r
+    for cmd in $history
+        if not string match -qr '^\s*r(\s|$)' -- $cmd
+            echo "→ $cmd"
+            eval $cmd
+            return
+        end
+    end
+end
